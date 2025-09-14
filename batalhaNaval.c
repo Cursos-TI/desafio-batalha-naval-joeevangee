918bb78e19755cc4c18d8edd573bff4e829bd3f2
Joel Jr <joe.evangee@gmail.com>
2025-09-14 13:25:46 +0000

    new commit

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,14 +1,53 @@
-#include <stdio.h>
+
 
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
-
-int main() {
+#include <stdio.h>
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+
+int main() {
+    // Declaração da matriz 10x10 usando números literais
+    char tabuleiro[10][10];
+    
+    // Inicialização do tabuleiro com 'A' para água
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = '0';
+        }
+    }
+
+    // POSICIONAR O NAVIO APÓS A INICIALIZAÇÃO
+    tabuleiro [5][6] = 'N';
+    tabuleiro [5][7] = 'N';
+    tabuleiro [5][8] = 'N';
+    // POSIÇÃO DO SEGUNDO NAVIO
+    tabuleiro [5][0] = 'N';
+    tabuleiro [6][0] = 'N';
+    tabuleiro [7][0] = 'N';
+    
+    // Impressão das coordenadas das colunas (A B C...)
+    printf("  ");
+    for (int j = 0; j < 10; j++) {
+        printf("%c ", 'A' + j);
+    }
+    printf("\n");
+    
+    // Impressão do tabuleiro com as coordenadas das linhas (0 1 2...)
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", i + 1); // Imprime o número da linha
+        for (int j = 0; j < 10; j++) {
+            printf("%c ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
+    
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -35,6 +74,3 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
-}
